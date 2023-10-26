961092e46a4b4e7b998e8c23a77d253a741af4f5
Madeliene <madelienestiller@gmail.com>
2023-10-26 11:30:21 +1000

    array map method

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(tutorial => {
+    let words = tutorial.split(' ');
+    let capitalisedWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return capitalisedWords.join(' ');
+  });
 }
+console.log(titleCased());
+
